b422933aa870b30443c8452708d1ee527bde80f2
Dawn Nguyen <55096320+DawnHNguyen@users.noreply.github.com>
2023-09-13 07:33:39 +0700

    Update PY01071 - PYTHON FILE.py

--- PY01071 - PYTHON FILE.py
+++ PY01071 - PYTHON FILE.py
@@ -1,17 +1,15 @@
-def check(n):
-    if len(n) < 3:
+def check(s):
+    if len(s) < 3:
         return False
-    n = n.lower();
-    if n[len(n) - 3:] != '.py':
+    if s[-3:] != '.py':
         return False
-    for i in n:
-        if ord(i) < ord('a') and ord(i) > ord('z') and ord(i) != ord('.') and ord(i) != ord('_'):
+    for i in s:
+        if (ord(i) < ord('a') or ord(i) > ord('z')) and ord(i) != ord('.') and ord(i) != ord('_'):
             return False
     return True
 
-#for i in range(int(input())):
-n = input()
-if check(n):
-    print("YES".lower())
+s = input().lower()
+if check(s):
+    print("yes")
 else:
-    print("NO".lower())
+    print("no")
